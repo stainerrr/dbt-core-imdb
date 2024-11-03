c7076dbb11db65d1eee5e566a07efb9a703957f2
stainr <stainr@outlook.com>
2024-11-03 16:15:21 +0000

    Added testing

--- models/staging/vw_stg__title_principals.sql
+++ models/staging/vw_stg__title_principals.sql
@@ -4,9 +4,9 @@ WITH vw_stg__title_principals AS (
         tconst AS t_const,
         TRY_CONVERT(INT, ordering) AS ordering,
         nconst                     AS n_const,
-        NULLIF(category, 'N\')     AS category,
-        NULLIF(job, 'N\')          AS job,
-        NULLIF(characters, 'N\')   AS characters,
+        NULLIF(category, '\N')     AS category,
+        NULLIF(job, '\N')          AS job,
+        NULLIF(characters, '\N')   AS characters,
         _etl_id,
         _loaded_at
     FROM {{ source("imdb_raw", "raw__title_principals") }}
